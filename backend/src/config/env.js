963b1b3f1862b40b5bcdcfd789fa8a56d7abcaf4
Rishi Ranjan <rishiofficial965@gmail.com>
2026-03-20 00:20:42 +0530

    Restore DNS override for MongoDB Atlas connection reliability

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,4 +1,7 @@
 import dotenv from "dotenv";
+import dns from "dns";
+
+// Fix for MongoDB Atlas DNS resolution issues on certain networks
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 dotenv.config();
-
